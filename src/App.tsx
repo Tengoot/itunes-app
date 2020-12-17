a1f0f848933f9cf81788688f6e41de50acfa0291
Bartosz Kowalski <tengoot@users.noreply.github.com>
2020-12-17 12:55:38 +0100

    Add saga and itunes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
 import styles from './App.module.css';
-import { Button, Stack } from '@chakra-ui/react';
-import { Counter } from './Counter/Counter';
+import { ITunes } from './itunes/ITunes';
 
 function App() {
   return (
     <div className={styles['App']}>
       <header className={styles['App-header']}>
-        {Counter()}
-        <Button colorScheme="blue">Hello world!</Button>
+        <ITunes />
       </header>
     </div>
   );
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,23 @@
 import { configureStore, Store } from '@reduxjs/toolkit';
 import { counterReducer } from './Counter/counterSlice';
+import createSagaMiddleware from 'redux-saga';
+import { all } from 'redux-saga/effects';
+import { counterSaga } from './Counter/counterSaga';
+import { itunesSaga } from './itunes/itunesSaga';
+import { itunesReducer } from './itunes/itunesSlice';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    itunes: itunesReducer,
   },
+  middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(function* () {
+  yield all([counterSaga(), itunesSaga()]);
 });
 
 type GetStoreState<S> = S extends Store<infer State, any> ? State : unknown;
